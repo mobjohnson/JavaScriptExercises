2e70f16947c6de806a15edd1430e5a33ba816106
mobjohnson <mobjohnson@gmail.com>
2015-03-12 20:04:33 -0400

    First three excercises on fruit_app

--- backbone/fruit_app/js/fruit.js
+++ backbone/fruit_app/js/fruit.js
@@ -1,1 +1,51 @@
+// // create the model
+// var Fruit = Backbone.Model.extend({});
+
+// // create a new instance of the model
+// var apple = new Fruit({
+//   type: 'apple',
+//   color: 'red',
+//   condition: 'shiny'
+// });
+
+// console.log(apple.get('color'));
+// console.set('condition', 'bruised');
+
+// **************************
+
+// // create the model
+// var Fruit = Backbone.Model.extend({
+//   description: function() {
+//     return this.get('color') + ', ' + this.get('condition') + ' ' + this.get('type');
+//   }
+// });
+
+// // create a new instance of the model
+// var apple = new Fruit({
+//   type: 'apple',
+//   color: 'red',
+//   condition: 'shiny'
+// });
+
+// console.log(apple.description());
+
+// **************************
+
 // create the model
+var Fruit = Backbone.Model.extend({
+  defaults: {
+    condition: 'perfect'
+  }
+});
+
+// create a new instance of the model
+var apple = new Fruit({
+  type: 'apple',
+  color: 'red',
+  condition: 'shiny'
+});
+
+console.log(apple.get('condition'));
+
+
+
